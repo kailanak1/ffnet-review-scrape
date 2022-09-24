3207af4e0bae5e488fd4df58c59658b0bee5eba6
Kailana Kahawaii <57549694+kailanak1@users.noreply.github.com>
2022-09-24 11:04:02 -0700

    Add usernames
    Adding "guest user" as a placeholder while I figure out how to scrape the table for guest names.

--- script.py
+++ script.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as Soup
 import re
+
 
 wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -24,17 +26,21 @@ try:
     reviews_tds = reviews_table.find_all('td')
     reviews = []
     for review_td in reviews_tds:
-        match = re.search(r'href="/u/(.*)/.*">.*</a>', str(review_td))
+        match = re.search(r'href="/u/(.*)/.*">(.*)</a>', str(review_td))
         if match is not None:
             user_id = int(match.groups()[0])
+            user_name = str(match.groups()[1])
         else:
             user_id = None
+            user_name = 'guest user'
+            # user_name = review_td.find_element(By.XPATH, '//*[@id="gui_table1i"]/tbody/tr[10]/td/text()')
         time = review_td.find('span', attrs={'data-xutime': True})
         time = int(time['data-xutime'])
         chapter = review_td.find('small').get_text()
         review = {
             'time': time,
             'chapter & date': chapter,
+            'user_name': user_name,
             'user_id': user_id,
             'text': review_td.div.text.encode('utf8')
         }
